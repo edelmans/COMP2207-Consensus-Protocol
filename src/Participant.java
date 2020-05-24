364231be462f78a73362ac5b6be06ca602229c44
edelmans <aedelman97@gmail.com>
2020-05-24 18:17:12 +0100

    update 005: Managed to get p2p connection. Now working on deciding the vote and then I have to figure out the whole thing about the rounds

--- src/Participant.java
+++ src/Participant.java
@@ -1,9 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.Buffer;
 import java.util.*;
 
 public class Participant {
@@ -14,7 +12,10 @@ public class Participant {
     private InputStreamReader in;
     private BufferedReader bf;
     private boolean connected = false;
-    private boolean p2pComplete = false;
+    private int round = 1;
+    private String outcomeVote = "";
+    private Vote chosenVote;
+    private int votesReceived = 0;
 
     private int cport, lport, pport, timeout;
     // <cport> port number that coordinator is listening on
@@ -23,10 +24,12 @@ public class Participant {
     // <timeout> timeout in milliseconds
     private ArrayList<Integer> otherParts = new ArrayList<>();
     private Set<String> options = new HashSet<String>();
-    private Vote chosenVote;
 
-    private ArrayList<Thread> participantServerThreads = new ArrayList<>();
-    private ArrayList<Thread> participantClientThreads = new ArrayList<>();
+
+    private ArrayList<Thread> participantSendThreads = new ArrayList<>();
+    private ArrayList<Thread> participantListenThreads = new ArrayList<>();
+
+    private HashMap<Socket, PrintWriter> sendHashMap = new HashMap<>();
 
     public Participant(String[] args) throws IOException {
         cport = Integer.parseInt(args[0]);
@@ -47,20 +50,142 @@ public class Participant {
     }
 
     void connectOthers(){
-        while(connected){
+        System.out.println("P" + pport +": Starting the voting process." );
+        //try to get it to listen to others, probably needs a ParticipantServer thread which will listen and accept any new connections
+        //try to get it to cast vote probably through creating a new socket for each otherport and sending a message
+
+        new Thread(this::getConnections).start();
+        makeConnections();
+        while(participantListenThreads.size() < otherParts.size() && sendHashMap.size() < otherParts.size()){
+            System.out.println("Waiting for all connections to be made");
+        }
+        sendVote();
+
+
+        while(votesReceived < otherParts.size()){
+            System.out.println("Not enough votes received yet");
             try{
-                if(p2pComplete == false){
-                    waitForConnections();
+                Thread.sleep(5000);
+            }catch (InterruptedException e){e.printStackTrace();}
+        }
+        votesReceived = 0;
+        decideOutcome();
+
+
+
+    }
+
+    void decideOutcome(){
+        String[] outcomeArr = outcomeVote.split(" ");
+        System.out.println(outcomeVote + ": Deciding the outcome");
+        ArrayList<Vote> votes = new ArrayList<>();
+        for(int i = 1; i < outcomeArr.length; i++){
+            votes.add(new Vote(Integer.parseInt(outcomeArr[i]), outcomeArr[i+1]));
+            i++;
+        }
+        //Array of votes for now
+
+
+
+
+    }
+
+
+
+    void getConnections(){
+        while(connected){
+            try {
+                Socket client = ss.accept();
+                System.out.println("P" + pport + ": New participant connected.");
+                ParticipantListenerThread thread = new ParticipantListenerThread(client);
+                synchronized (participantListenThreads){
+                    participantListenThreads.add(thread);
+                    System.out.println("P" + pport + ": New participant added to list of threads.");
                 }
+                thread.start();
+            }catch (IOException e){System.out.println("IOException at getConnections");}
+
+        }
+    }
+
+    private class ParticipantListenerThread extends Thread{
+        private Socket client;
+        private BufferedReader in;
+        private boolean running = true;
+        private int clientPort;
+
+        ParticipantListenerThread(Socket socket) throws IOException {
+            client = socket;
+            this.in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        }
+
+        public void run(){
+            String msg;
+            String[] msgArr;
+            try{
+                while(running){
+                    msg = in.readLine();
+                    if(msg == null){
+                        System.out.println("P" + pport + ": Connection lost with " + clientPort);
+                        client.close();
+                        in.close();
+                        synchronized (participantListenThreads){
+                            participantListenThreads.remove(this);
+                        }
+                        if(participantListenThreads.size() == 0){
+                            System.out.println("P" + pport + ": No more participants");
+                            System.exit(1);
+                        }
+                        running = false;
+                        return;
+                    }
+                    msgArr = msg.split(" ");
+                    if(msgArr[0].equals("VOTE")){
+                        clientPort = Integer.parseInt(msgArr[1]);
+                        System.out.println("P" + pport + ": VOTE from " + clientPort);
+                        System.out.println("P" + pport + ": Registering vote");
+
+                        synchronized (outcomeVote) {
+                            outcomeVote += " " + clientPort + " " + msgArr[2];
+                            System.out.println("New outcomeVote: " + outcomeVote);
+                        }
+                        votesReceived++;
+                    }else{
+                        System.out.println("P" + pport + ": Unknown message: " + msg);
+                    }
+
+                }
+
+            }catch (IOException e){
+                System.err.println("Caught I/O Exception");
             }
         }
     }
 
-    void waitForConnections(){
-        for(int p : otherParts){
-
+    void makeConnections(){
+        for(int i : otherParts){
+            try{
+                Socket sendSocket = new Socket("localhost", i);
+                PrintWriter sendOut = new PrintWriter(new OutputStreamWriter(sendSocket.getOutputStream()),true);
+                synchronized (sendHashMap){
+                    System.out.println("P" + pport +": Connection made with " + i + " and added to sendHashMap");
+                    sendHashMap.put(sendSocket, sendOut);
+                }
+            }catch (IOException e){System.out.println("P" + pport +": Can't make a connection with another participant " + e);}
         }
     }
+
+    void sendVote(){
+//        String vote = "VOTE " + pport + " " + chosenVote;
+        synchronized (sendHashMap) {
+            for (HashMap.Entry<Socket, PrintWriter> entry : sendHashMap.entrySet()) {
+                entry.getValue().println(outcomeVote);
+            }
+        }
+        System.out.println("Votes sent to all the other participants");
+        round++;
+    }
+
 
     void waitforDetails(){
         try {
@@ -116,26 +241,9 @@ public class Participant {
 
         int i = rand.nextInt(array.length);
         chosenVote = new Vote(pport, array[i]);
+        outcomeVote = "VOTE " + pport + " " + chosenVote.getVote();
         System.out.println("P" + pport +": Chosen vote: " + chosenVote.getVote());
     }
-
-    private class ParticipantClientThread extends Thread{
-        private Socket client;
-        private int serverPport;
-        private BufferedReader in;
-        private PrintWriter out;
-        private boolean running = true;
-
-        ParticipantClientThread(Socket client) throws IOException {
-
-
-        }
-
-        public void run(){
-
-        }
-    }
-
 
 
     public static void main(String[] args) throws IOException{
@@ -157,7 +265,6 @@ public class Participant {
         participant.chooseVote();
 
         participant.connectOthers();
-        System.err.println("Ready to vote");
 
 
 
